0cf774f1166d81ca0fde637165c5b582692c3dc3
WuGuMoFang <wugumofang@wugumofangdemac-mini.local>
2016-04-12 15:13:18 +0800

    Initial Commit

---
+++ AFNDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNDemo
+//
+//  Created by WuGuMoFang on 16/4/12.
+//  Copyright © 2016年 wugumofang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AFNDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AFNDemo
+//
+//  Created by WuGuMoFang on 16/4/12.
+//  Copyright © 2016年 wugumofang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
